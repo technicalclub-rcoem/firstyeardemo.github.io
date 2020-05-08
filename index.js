bf02416903485e98c4cfbe2403239d3fb94587ea
technicalclub-rcoem <technicalclub-rcoem>
2020-05-08 11:54:16 +0530

    rotation cross created

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hello from index.js");
+Stage(function (stage) {
+	var number=1;
+	stage.viewbox(50,50).pin({'handle':-0.5});
+
+	var sign =Stage.image('x').appendTo(stage).pin('handle',0.5);
+
+	sign.on('click',function(){
+		this.tween().ease('out').pin('rotation',++number*Math.PI);
+	});
+});
+
+Stage({
+
+	textures : {
+		'x': Stage.canvas(function(ctx){
+			ratio=10;
+			this.size(10,10,ratio);
+			ctx.scale(ratio,ratio);
+
+			ctx.moveTo(0,0);
+			ctx.lineTo(10,10);
+			ctx.moveTo(10,0);
+			ctx.lineTo(0,10);
+
+			ctx.strokeWidth=1;
+			ctx.stroke();
+		})
+	}
+});
